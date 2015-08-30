6719464805114b0d41b8a3faa494ee1d3bb4b2f9
Ajay Tungare <ajay.tungare@gmail.com>
2015-08-30 15:40:43 -0700

    step 1

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,13 @@
-'use strict';
+"use strict";
 
 var React = require('react');
+
+var Box = React.createClass({
+  render: function () {
+    return (
+      <button>X</button>
+    );
+  }
+});
+
+React.render(<Box/>, document.body);
