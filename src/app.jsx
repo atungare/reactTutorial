9fe74521211d1494331f06d7e58af68245afd19d
Ajay Tungare <ajay.tungare@gmail.com>
2015-08-30 15:54:00 -0700

    step 4

--- src/app.jsx
+++ src/app.jsx
@@ -10,13 +10,25 @@ var box_style = {
 var Box = React.createClass({
   render: function () {
     return (
-      <button style={box_style}>{this.state.value}</button>
+      <button style={ box_style }>{ this.state.value }</button>
     );
   },
   getInitialState: function () {
     return {
       value: 'X'
     };
+  },
+  updateTextValue: function () {
+    var new_state = {};
+    if (this.state.value === 'X') {
+      new_state.value = "O"
+    } else if (this.state.value === 'O') {
+      new_state.value = "X"
+    }
+    this.setState(new_state)
+  },
+  componentDidMount: function () {
+    setInterval(this.updateTextValue, 300);
   }
 });
 
